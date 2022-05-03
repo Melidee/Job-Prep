5db5e3440e9c12e9d544f17304996430d5b16488
Melidee <86878748+Melidee@users.noreply.github.com>
2022-05-03 16:17:01 -0500

    created auth user model

--- djcrm/leads/models.py
+++ djcrm/leads/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
 
 class Agent(models.Model):
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=60)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
-
