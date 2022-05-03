55035c3e3fad1d737df1228de274881549ab5351
Melidee <86878748+Melidee@users.noreply.github.com>
2022-05-02 22:38:33 -0500

    Added agent model and agent foreign key for lead

--- djcrm/leads/models.py
+++ djcrm/leads/models.py
@@ -1,18 +1,12 @@
 from django.db import models
 
+class Agent(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+
 class Lead(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
 
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=(
-        ('YouTube', 'YouTube'),
-        ('Google', 'Google'),
-        ('Newsletter', 'Newsletter'),
-    ), max_length=100)
-
-    profile_pic = models.ImageField(blank=True, null=True)
-    special_files = model.models.FileField(blank=True, null=True)
-
-    
